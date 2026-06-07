fff7fe09af44a33bea7d7fb511d9714224780fe1
HD1305CODER <harshdevera@gmail.com>
2026-06-07 10:24:30 +0000

    datatype

---
+++ 1_basic/datatype.js
@@ -0,0 +1,21 @@
+//non primitive data type
+
+// int     let age=39
+// bigint
+// string =>  let name="harsh"
+// boolean => true/false
+// null -> standalone Value  
+// undefined => not assigned variable
+// symbol => for uniqueness
+
+//primitive datatype
+// object
+
+console.log( typeof undefined)     //undefined
+console.log( typeof null)     //object
+
+
+
+// alert("hii")   this is only used in browser console. we are using nodejs
+
+"using strict"  // it will write in starting . its meaning is in this code we are using newer js code
