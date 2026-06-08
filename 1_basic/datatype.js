22af565836f108df0eb54952fc1301ed2f0425e2
HD1305CODER <harshdevera@gmail.com>
2026-06-08 12:38:28 +0000

    add about memory

--- 1_basic/datatype.js
+++ 1_basic/datatype.js
@@ -1,4 +1,4 @@
-//non primitive data type
+//primitive data type
 
 // int     let age=39(or number in js decimal and integer bith are same )
 // bigint
@@ -8,7 +8,7 @@
 // undefined => not assigned variable
 // symbol => for uniqueness
 
-//primitive datatype
+//non-primitive datatype
 // object
 
 console.log( typeof undefined)     //undefined
@@ -19,3 +19,11 @@ console.log( typeof null)     //object
 // alert("hii")   this is only used in browser console. we are using nodejs
 
 "using strict"  // it will write in starting . its meaning is in this code we are using newer js code
+
+
+
+//---------********* MEMORY STORING ********----------//
+
+//STACK=> USEND BY PRIMITIVE DATATYPE : in stack value is passed by vlaue means we get an copy that means when we made change in it than the original variable is not get change: 
+//HEAP=> USED by non primitivr datatype : in heap value is passed by reference means we get actual add of that variable and changes are made in actual variable:
+
